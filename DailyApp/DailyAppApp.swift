b80f41cad2821ac7cf942f7f44b9949cee17c620
Antony Ashwin <antonyashwin555@gmail.com>
2025-10-12 18:30:29 +0530

    Initial commit

---
+++ DailyApp/DailyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyAppApp.swift
+//  DailyApp
+//
+//  Created by Ashwin, Antony on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyAppTests/DailyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyAppTests.swift
+//  DailyAppTests
+//
+//  Created by Ashwin, Antony on 12/10/25.
+//
+
+import Testing
+@testable import DailyApp
+
+struct DailyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
